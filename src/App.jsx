1d853e5da58cf828db60816bf41dc037948f2b1f
Alireza Haji Hosseini <a1ire24@yahoo.com>
2024-07-04 17:46:58 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "src/components/Header.jsx";
+import Footer from "src/components/Footer.jsx";
+import Shops from "src/components/Shops.jsx";
+
+import "./App.css";
+
+function App() {
+	return (
+		<div id="main">
+			<Header/>
+			<Shops/>
+			<Footer/>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div>
+            <img style={{width: "100%"}} src="/assets/images/header.jpg"/>
+        </div>
+    )
+}
